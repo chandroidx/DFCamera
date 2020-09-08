56c4e6212bd879c677fe551bdfb2eae29f1224cb
Mephrine <dthwrld@gmail.com>
2020-09-08 18:58:18 +0900

    focushanlder null 관련 처리

--- DFCamera/src/main/java/com/deepfine/dfcamera/FocusHandler.kt
+++ DFCamera/src/main/java/com/deepfine/dfcamera/FocusHandler.kt
@@ -25,80 +25,81 @@ internal class FocusHandler {
             )
             return
         }
-
-        // cancel any existing AF trigger (repeated touches, etc.)
-        captureRequestBuilder!!.set(
-            CaptureRequest.CONTROL_AF_TRIGGER,
-            CameraMetadata.CONTROL_AF_TRIGGER_CANCEL
-        )
-        captureRequestBuilder.set(
-            CaptureRequest.CONTROL_AF_MODE,
-            CaptureRequest.CONTROL_AF_MODE_OFF
-        )
-
-        // add a new AF trigger with focus region
-        if (focusArea != null) {
-            val rectangle =
-                MeteringRectangle(focusArea, MeteringRectangle.METERING_WEIGHT_MAX - 1)
-            captureRequestBuilder.set(
-                CaptureRequest.CONTROL_AF_REGIONS,
-                arrayOf(rectangle)
+        captureRequestBuilder?.let {
+            // cancel any existing AF trigger (repeated touches, etc.)
+            it.set(
+                CaptureRequest.CONTROL_AF_TRIGGER,
+                CameraMetadata.CONTROL_AF_TRIGGER_CANCEL
             )
-        }
-        captureRequestBuilder.set(
-            CaptureRequest.CONTROL_MODE,
-            CameraMetadata.CONTROL_MODE_AUTO
-        )
-        captureRequestBuilder.set(
-            CaptureRequest.CONTROL_AF_MODE,
-            CaptureRequest.CONTROL_AF_MODE_AUTO
-        )
-        captureRequestBuilder.set(
-            CaptureRequest.CONTROL_AF_TRIGGER,
-            CameraMetadata.CONTROL_AF_TRIGGER_START
-        )
-        captureRequestBuilder.setTag("FOCUS_TAG") // we'll capture this later for resuming the preview
-        try {
-            val focusCallbackHandler: CaptureCallback = object : CaptureCallback() {
-                override fun onCaptureCompleted(
-                    session: CameraCaptureSession,
-                    request: CaptureRequest,
-                    result: TotalCaptureResult
-                ) {
-                    isFocusProcessing = false
-                    if (request.tag === "FOCUS_TAG") {
-                        // the focus trigger is complete, clear AF trigger
-                        captureRequestBuilder.set(CaptureRequest.CONTROL_AF_TRIGGER, null)
-                        callback.onFinish(null)
+            it.set(
+                CaptureRequest.CONTROL_AF_MODE,
+                CaptureRequest.CONTROL_AF_MODE_OFF
+            )
+
+            // add a new AF trigger with focus region
+            if (focusArea != null) {
+                val rectangle =
+                    MeteringRectangle(focusArea, MeteringRectangle.METERING_WEIGHT_MAX - 1)
+                it.set(
+                    CaptureRequest.CONTROL_AF_REGIONS,
+                    arrayOf(rectangle)
+                )
+            }
+            it.set(
+                CaptureRequest.CONTROL_MODE,
+                CameraMetadata.CONTROL_MODE_AUTO
+            )
+            it.set(
+                CaptureRequest.CONTROL_AF_MODE,
+                CaptureRequest.CONTROL_AF_MODE_AUTO
+            )
+            it.set(
+                CaptureRequest.CONTROL_AF_TRIGGER,
+                CameraMetadata.CONTROL_AF_TRIGGER_START
+            )
+            it.setTag("FOCUS_TAG") // we'll capture this later for resuming the preview
+            try {
+                val focusCallbackHandler: CaptureCallback = object : CaptureCallback() {
+                    override fun onCaptureCompleted(
+                        session: CameraCaptureSession,
+                        request: CaptureRequest,
+                        result: TotalCaptureResult
+                    ) {
+                        isFocusProcessing = false
+                        if (request.tag === "FOCUS_TAG") {
+                            // the focus trigger is complete, clear AF trigger
+                            it.set(CaptureRequest.CONTROL_AF_TRIGGER, null)
+                            callback.onFinish(null)
+                        }
+                    }
+
+                    override fun onCaptureFailed(
+                        session: CameraCaptureSession,
+                        request: CaptureRequest,
+                        failure: CaptureFailure
+                    ) {
+                        isFocusProcessing = false
+                        callback.onFinish(
+                            Error(
+                                Error.ERROR_CAMERA,
+                                "focus failed"
+                            )
+                        )
                     }
                 }
-
-                override fun onCaptureFailed(
-                    session: CameraCaptureSession,
-                    request: CaptureRequest,
-                    failure: CaptureFailure
-                ) {
-                    isFocusProcessing = false
-                    callback.onFinish(
-                        Error(
-                            Error.ERROR_CAMERA,
-                            "focus failed"
-                        )
+                captureSession?.capture(it.build(), focusCallbackHandler, null)
+            } catch (e: CameraAccessException) {
+                e.printStackTrace()
+                callback.onFinish(
+                    Error(
+                        Error.ERROR_CAMERA,
+                        e
                     )
-                }
-            }
-            captureSession?.capture(captureRequestBuilder.build(), focusCallbackHandler, null)
-        } catch (e: CameraAccessException) {
-            e.printStackTrace()
-            callback.onFinish(
-                Error(
-                    Error.ERROR_CAMERA,
-                    e
                 )
-            )
-            return
+                return
+            }
+            isFocusProcessing = true
         }
-        isFocusProcessing = true
     }
 
     internal interface Callback {
